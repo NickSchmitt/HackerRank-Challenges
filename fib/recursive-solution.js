c8555a0d2cd972cbab4d001ac79d26bf4798b89e
nickschmitt <nickschmitt@gmail.com>
2021-03-03 11:52:36 -0800

    .

--- fib/recursive-solution.js
+++ fib/recursive-solution.js
@@ -1,16 +1,18 @@
-// const fib = (n) => {
-// 	if (n <= 2) return 1
-// 	return fib(n-1) + fib(n-2)
-// }
+const fib = (n) => {
+	// if (n <= 2) return 1
+	console.log(n-1)
+	console.log(n-2)
+	return fib(n-1) + fib(n-2)
+}
 
 // memoization 
 
-const fib = (n, hash={}) => {
-	if (n in hash) return hash[n]
-	if (n<= 2) return 1
-	hash[n] = fib(n-1, hash) + fib(n-2, hash)
-	return hash[n]
-}
+// const fib = (n, hash={}) => {
+// 	if (n in hash) return hash[n]
+// 	if (n<= 2) return 1
+// 	hash[n] = fib(n-1, hash) + fib(n-2, hash)
+// 	return hash[n]
+// }
 
 let num1 = 5
 let num2 = 50
@@ -19,7 +21,7 @@ console.time(num1.toString())
 fib(num1)
 console.timeEnd(num1.toString())
 
-console.time(num2.toString())
-fib(num2)
-console.timeEnd(num2.toString())
+// console.time(num2.toString())
+// fib(num2)
+// console.timeEnd(num2.toString())
 
